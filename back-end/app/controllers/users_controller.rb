563b48dca6c5b280b95f3046c5bfb54dd4771623
nupogodi1989 <coffeestain@protonmail.com>
2019-03-29 20:17:39 -0600

    send as hash

--- back-end/app/controllers/users_controller.rb
+++ back-end/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
-    render json: User.all.map(&:as_json).sort_by { |u| u[:name].downcase }
+    render json: { Users: User.all.map(&:as_json).sort_by do |u|
+      u[:name].downcase
+    end }
   end
 end
