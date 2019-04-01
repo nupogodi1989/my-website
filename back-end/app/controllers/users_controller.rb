9ac16353ce59c895dcab395fb0e5d384a9555cd7
nupogodi1989 <coffeestain@protonmail.com>
2019-04-01 14:27:11 -0600

    send as array of hashes

--- back-end/app/controllers/users_controller.rb
+++ back-end/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
-    render json: { Users: User.all.map(&:as_json).sort_by do |u|
+    render json: User.all.map(&:as_json).sort_by do |u|
       u[:name].downcase
-    end }
+    end
   end
 end
