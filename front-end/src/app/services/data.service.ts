a5315474f644696701a8d6d92d3e4b6df21d0271
nupogodi1989 <coffeestain@protonmail.com>
2019-04-01 17:22:25 -0600

    move data service to services directory, no need for rxjs .map since HttpClient returns json already

--- front-end/src/app/services/data.service.ts
+++ front-end/src/app/services/data.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
@@ -8,7 +7,7 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   retrieveAllUsers() {
-    return this.http.get('http://localhost:3000/users.json').pipe(map(res => res));
+    return this.http.get('http://localhost:3000/users.json');
   }
 
 }
